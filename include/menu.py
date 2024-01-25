5a1928dd6ed0f74533c1169fd861c0cfc2475d2b
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-01-25 17:00:20 +0100

    changement du pokedex + ajout d'un fichier.json + ajout des ressources dans assets

--- include/menu.py
+++ include/menu.py
@@ -1,10 +1,8 @@
 import pygame, sys
 from tkinter import Tk
 from button import Button
-from pokedex import PokedexApp
+from pokedex import display_pokedex
 
-root = Tk()
-app = PokedexApp(root)
 pygame.init()
 
 FPS = 30
@@ -12,7 +10,7 @@ FPS = 30
 
 pygame.display.set_caption("Pokemon")
 
-BG = pygame.image.load("assets/image/background.jpg")
+BG = pygame.image.load("assets/image/background/background.jpg")
 LARGEUR, HAUTEUR = BG.get_size()
 SCREEN = pygame.display.set_mode((LARGEUR, HAUTEUR))
 WIDTH = BG.get_width()
@@ -49,31 +47,13 @@ def play():
 
 def pokedex():
     while True:
-        POKEDEX_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.fill("White")
-
-        POKEDEX_TEXT = get_font(30).render("This is the POKEDEX screen.", True, "Black")
-        POKEDEX_RECT = POKEDEX_TEXT.get_rect(center=(LARGEUR/2, HAUTEUR/2))
-        SCREEN.blit(POKEDEX_TEXT, POKEDEX_RECT)
-
-        root.mainloop()
-
-        POKEDEX_BACK = Button(image=None, pos=(LARGEUR/2, HAUTEUR/1.4),
-                            text_input="BACK", font=get_font(45), base_color="Black", hovering_color="#b68f40")
-
-        POKEDEX_BACK.changeColor(POKEDEX_MOUSE_POS)
-        POKEDEX_BACK.update(SCREEN)
-
+        display_pokedex()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if POKEDEX_BACK.checkForInput(POKEDEX_MOUSE_POS):
-                    main_menu()
-
-        pygame.display.update()
+            if event.type == pygame.K_ESCAPE:
+                main_menu()
 
 
 def options():
@@ -109,13 +89,13 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/image/Play Rect.png"), pos=(LARGEUR/2, HAUTEUR/2.4),
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/image/background/Play Rect.png"), pos=(LARGEUR/2, HAUTEUR/2.4),
                             text_input="PLAY", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
-        POKEDEX_BUTTON = Button(image=pygame.image.load("assets/image/Pokedex.png"), pos=(LARGEUR/2, HAUTEUR/1.8),
+        POKEDEX_BUTTON = Button(image=pygame.image.load("assets/image/background/Pokedex.png"), pos=(LARGEUR/2, HAUTEUR/1.8),
                                 text_input="POKEDEX", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/image/PLay Rect.png"), pos=(LARGEUR/2, HAUTEUR/1.45),
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/image/background/PLay Rect.png"), pos=(LARGEUR/2, HAUTEUR/1.45),
                             text_input="OPTIONS", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/image/Play Rect.png"), pos=(LARGEUR/2, HAUTEUR/1.2),
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/image/background/Play Rect.png"), pos=(LARGEUR/2, HAUTEUR/1.2),
                             text_input="QUIT", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
 
         for button in [PLAY_BUTTON, POKEDEX_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
--- include/pokedex.py
+++ include/pokedex.py
@@ -1,55 +1,115 @@
-import requests
-from tkinter import Tk, Label, Button, PhotoImage
-from io import BytesIO
-from PIL import Image, ImageTk
+import json
+import pygame
+from pygame.locals import *
+import time
 
-class PokedexApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("")
-        self.root.configure(bg="white")
-        self.height = 200
-        self.width = 200
-        self.x = (root.winfo_screenwidth()//2)-(self.width//2)
-        self.y = (root.winfo_screenheight()//2)-(self.height//2)
-        self.root.geometry("{}x{}+{}+{}".format(self.width, self.height, self.x, self.y))
+KEY_REPEAT_DELAY = 0.1
 
-        self.current_pokemon_id = 1
+def display_pokedex():
+    pygame.init()
 
-        self.name_label = Label(root, text="", font=("./assets/font/font.ttf", 16), fg="#a76ff0", bg="white")
-        self.name_label.pack()
+    WINDOW_WIDTH = 1024
+    WINDOW_HEIGHT = 1024
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("POKEDEX")
 
-        self.image_label = Label(root, bg="white")
-        self.image_label.pack()
+    with open("pokedex_data.json", "r") as json_file:
+        pokemon_data = json.load(json_file)
 
-        self.load_pokemon_data()
-        self.update_ui()
+    pokemon_sprites = [pygame.image.load(pokemon["sprite"]) for pokemon in pokemon_data]
+    pokemon_sprites_back = [pygame.image.load(pokemon["back"]) for pokemon in pokemon_data]
+    pokemon_sprites_footprint = [pygame.image.load(pokemon["footprint"]) for pokemon in pokemon_data]
 
-        next_button = Button(root, text="Next Pokemon", command=self.next_pokemon, font=("./assets/font/font.ttf"), fg="#b68f40", bg="white", borderwidth=0)
-        next_button.pack()
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
 
-    def load_pokemon_data(self):
-        api_url = f"https://pokeapi.co/api/v2/pokemon/{self.current_pokemon_id}/"
-        response = requests.get(api_url)
-        data = response.json()
+    font_big = pygame.font.Font("assets/font/Kenzo.otf", 48)
+    font_small = pygame.font.Font("assets/font/Kenzo.otf", 36)
+    font_extra = pygame.font.Font("assets/font/Kenzo.otf", 668)
 
-        self.pokemon_name = data['name'].capitalize()
+    running = True
+    selected_index = 0
+    key_repeat_time = 0
 
-        sprite_url = data['sprites']['front_default']
-        sprite_response = requests.get(sprite_url)
-        image_data = Image.open(BytesIO(sprite_response.content))
-        self.pokemon_image = ImageTk.PhotoImage(image_data)
+    while running:
+        current_time = time.time()
 
-    def update_ui(self):
-        self.name_label.config(text=self.pokemon_name)
-        self.image_label.config(image=self.pokemon_image)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_UP:
+                    selected_index = max(0, selected_index - 1)
+                    key_repeat_time = current_time + KEY_REPEAT_DELAY
+                elif event.key == K_DOWN:
+                    selected_index = min(len(pokemon_data) - 1, selected_index + 1)
+                    key_repeat_time = current_time + KEY_REPEAT_DELAY
 
-    def next_pokemon(self):
-        self.current_pokemon_id += 1
-        self.load_pokemon_data()
-        self.update_ui()
+        keys = pygame.key.get_pressed()
 
-if __name__ == "__main__":
-    root = Tk()
-    app = PokedexApp(root)
-    root.mainloop()
+        if keys[K_UP] and current_time > key_repeat_time:
+            selected_index = max(0, selected_index - 1)
+            key_repeat_time = current_time + KEY_REPEAT_DELAY
+        elif keys[K_DOWN] and current_time > key_repeat_time:
+            selected_index = min(len(pokemon_data) - 1, selected_index + 1)
+            key_repeat_time = current_time + KEY_REPEAT_DELAY
+
+        window.fill(WHITE)
+
+        arriere_plan = pygame.image.load('assets/image/background/bg2.jpg').convert()
+        arriere_plan = pygame.transform.scale(arriere_plan, (WINDOW_WIDTH, WINDOW_HEIGHT))
+        arriere_plan.set_alpha(180)
+        window.blit(arriere_plan, (0,0))
+
+        if 0 <= selected_index < len(pokemon_data):
+            selected_pokemon = pokemon_data[selected_index]
+            name = selected_pokemon["name"]
+            type = selected_pokemon["type"]
+            defense = selected_pokemon["defense"]
+            attack = selected_pokemon["attack"]
+            hp = selected_pokemon["hp"]
+            att_spe = selected_pokemon["special_attack"]
+            def_spe = selected_pokemon["special_defense"]
+            speed = selected_pokemon["speed"]
+
+            text = font_extra.render(f"#{selected_pokemon['id']}", True, BLACK)
+            text.set_alpha(80)
+            window.blit(text, (0,  WINDOW_HEIGHT/4))
+
+            window.blit(pokemon_sprites[selected_index], (50, 50))
+            window.blit(pokemon_sprites_footprint[selected_index], (38, 140))
+            window.blit(pokemon_sprites_back[selected_index], (WINDOW_WIDTH/2.5, WINDOW_HEIGHT/2.5))
+            text = font_big.render(f"{name}", True, BLACK)
+            window.blit(text, (180, 50))
+
+
+            text = font_small.render(f"Type: {type}", True, BLACK)
+            window.blit(text, (180, 120))
+
+            text = font_small.render(f"Taille: {selected_pokemon.get('height', 'N/A')/10}M", True, BLACK)
+            window.blit(text, (180, 160))
+
+            text = font_small.render(f"Poids: {selected_pokemon.get('weight', 'N/A')/10}KG", True, BLACK)
+            window.blit(text, (180, 200))
+
+            text = font_small.render(f"Talents: {', '.join(selected_pokemon.get('abilities', ['N/A']))}", True, BLACK)
+            window.blit(text, (180, 240))
+
+            additional_info = [
+                f"HP: {hp}",
+                f"Attaque: {attack}",
+                f"Defense: {defense}",
+                f"Attaque Speciale: {att_spe}",
+                f"Defense Speciale: {def_spe}",
+                f"Vitesse: {speed}"
+            ]
+
+            for i, info in enumerate(additional_info):
+                text = font_small.render(info, True, BLACK)
+                window.blit(text, (180, WINDOW_HEIGHT/1.35 + i * 40))
+
+            text = font_big.render("STATS", True, BLACK)
+            window.blit(text, (50,WINDOW_HEIGHT/1.35))
+
+        pygame.display.flip()
+    pygame.quit()
